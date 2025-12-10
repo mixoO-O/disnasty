88243d4ea7c091c45c0ab441c393fc3a2c4813ba
mrojasm <mrojasm@previred.com>
2025-12-10 19:52:41 -0300

    x

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -39,7 +39,6 @@ export default function Home({ params: { locale } }: { params: { locale: string 
                 <ClientCarousel />
             </ScrollReveal> */}
 
-
       {/* <ScrollReveal>
                 <Testimonials />
                 </ScrollReveal> */}
